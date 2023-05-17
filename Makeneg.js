4c5431208e437c86e704909c953f6c053e37f120
tdreams <fabriels@gmail.com>
2023-05-17 07:46:14 -0700

    day8

--- Makeneg.js
+++ Makeneg.js
@@ -6,5 +6,5 @@ makeNegative(-5);   // return -5
 makeNegative(0);    // return 0
 makeNegative(0.12); // return -0.12 */
 function makeNegative(num) {
-  return num === 0 ? num : -Math.abs(num);
+  return num < 0 ? num : -num;
 }
