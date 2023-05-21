fd32ddca64b8bad0c32fce36e4fbbc61ef5ac3e4
tdreams <fabriels@gmail.com>
2023-05-20 21:25:39 -0700

    day10

---
+++ filter.js
@@ -0,0 +1,6 @@
+//20/05/2023
+/* In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out. */
+
+function filter_list(l) {
+  return l.filter((elt) => typeof elt === "number");
+}
