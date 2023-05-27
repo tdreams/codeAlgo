2c5c67e057f77e16554e6a8b4fad9de1c88a3a9a
tdreams <fabriels@gmail.com>
2023-05-26 18:52:03 -0700

    bmi

--- bmi.js
+++ bmi.js
@@ -8,8 +8,13 @@ if bmi <= 25.0 return "Normal"
 if bmi <= 30.0 return "Overweight"
 
 if bmi > 30 return "Obese" */
-function bmi(weight, height) {
+const bmi = (weight, height) => {
   const bmi = weight / (height * height);
-
-  return bmi <= 18.5 ? "Underweight" : bmi <= 25.0 ? "Normal" : "Obese";
-}
+  return bmi <= 18.5
+    ? "Underweight"
+    : bmi <= 25.0
+    ? "Normal"
+    : bmi <= 30
+    ? "Overweight"
+    : "Obese";
+};
