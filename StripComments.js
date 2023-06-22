ea4a72b8d5cea7c78e85d1455b4d0c996ea0e3bb
tdreams <fabriels@gmail.com>
2023-06-21 22:46:08 -0700

    stripComments

---
+++ StripComments.js
@@ -0,0 +1,25 @@
+/* Complete the solution so that it strips all text that follows any of a set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
+
+Example:
+
+Given an input string of:
+
+apples, pears # and bananas
+grapes
+bananas !apples
+The output expected would be:
+
+apples, pears
+grapes
+bananas */
+
+const solution = (input, markers) => {
+  const regex = new RegExp(`\\s*[${markers.join("")}].*`, "g");
+  return input.replace(regex, "");
+};
+console.log(
+  solution("apples, plums % and bananas\npears\noranges !applesauce", [
+    "%",
+    "!",
+  ])
+);
