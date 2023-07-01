15708c0834fb666bc69a92bf95aeec9efcface47
tdreams <fabriels@gmail.com>
2023-06-30 21:51:24 -0700

    update

--- RGBToHexConversion.js
+++ RGBToHexConversion.js
@@ -9,26 +9,17 @@ rgb(255, 255, 300) // returns FFFFFF
 rgb(0,0,0) // returns 000000
 rgb(148, 0, 211) // returns 9400D3 */
 
-const rgb1 = (r, g, b) => {
-  if (r > 255) {
-    r = 255;
-  }
-  if (g > 255) {
-    g = 255;
-  }
-  if (b > 255) {
-    b = 255;
-  }
-
-  return r.toString(16) + g.toString(16) + b.toString(16);
-};
-
 const rgb = (r, g, b) => {
   r = Math.max(0, Math.min(255, r));
   g = Math.max(0, Math.min(255, g));
   b = Math.max(0, Math.min(255, b));
+  const componentToHex = (c) => {
+    const hex = c.toString(16);
+    return hex.length === 1 ? "0" + hex : hex;
+  };
+  const red = componentToHex(r);
+  const green = componentToHex(g);
+  const blue = componentToHex(b);
 
   return (red + green + blue).toUpperCase();
 };
-
-console.log(rgb(300, 255, 255));
