4078b0ee573c89007b52d67e1bfed1cd19960e1d
fab <fabriels@gmail.com>
2023-09-13 19:22:23 -0700

    favoriteBooks

--- bubbleSort.js
+++ bubbleSort.js
@@ -17,7 +17,7 @@ function bubbleSort(array) {
     for (let i = 1; i < array.length; i += 1) {
       if (array[i - 1] > array[i]) {
         done = false;
-        var tmp = array[i - 1];
+        let tmp = array[i - 1];
         array[i - 1] = array[i];
         array[i] = tmp;
       }
